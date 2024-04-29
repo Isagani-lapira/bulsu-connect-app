c80987118ebc274f8b047db30d20ae1a10c26fa3
Isagani Lapira <lapiraisagani@gmail.com>
2024-04-29 09:36:46 +0800

    Add: initial colors

---
+++ bulsuconnect/lib/theme/color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primary = Color(0xFFC41E3A);
+  static Color primaryShade = primary.withOpacity(0.5);
+  static const Color textColor = Color(0xFF1E1E1E);
+  static Color hintColor = Colors.grey.shade300;
+  static const Color iconColor = Color(0xFF757575);
+  static const Color disabledColor = Color.fromARGB(255, 80, 80, 80);
+  static const Color accentColor = Color(0xFF3669C9);
+  static const Color focusedButton = Color.fromARGB(255, 250, 129, 121);
+}
